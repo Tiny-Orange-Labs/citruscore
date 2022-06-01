377ba305365107e498889554a916878fcb618a91
oliverjessner <oliver.jessner@gmail.com>
2022-06-01 14:29:10 +0200

    adding a modal window

--- dist/dev/bundle.js
+++ dist/dev/bundle.js
@@ -987,15 +987,98 @@ null == n || n({ LitElement: s });
 ).push('3.2.0');
 
 class AppLayout extends s {
-    static styles = r$2`:host{display:grid;grid-template-rows:var(--nav-width) 1fr;gap:var(--gap);max-height:100vh;height:100vh}nav{background-color:var(--color-primary)}i{color:var(--color-on-primary)}`;
+    createRenderRoot() {
+        return this;
+    }
+
+    openNavi() {
+        const sideNavi = this.querySelector('nav');
+        const isOpen = sideNavi.getAttribute('data-open');
+
+        if (isOpen === 'true') {
+            sideNavi.setAttribute('data-open', 'false');
+            return document.body.style.setProperty('--side-navi-width', '0');
+        }
+
+        sideNavi.setAttribute('data-open', 'true');
+        document.body.style.setProperty('--side-navi-width', '365px');
+    }
 
     constructor() {
         super();
     }
 
     render() {
-        return $`<link rel="stylesheet" href="../assets/css/fontawesome.min.css"><link rel="stylesheet" href="../assets/css/solid.css"><nav><i class="fa-solid fa-bars"></i></nav><main><div></div><div></div></main>`;
+        return $`<header><i @click="${this.openNavi}" class="fa-solid fa-bars"></i></header><nav data-open="true"></nav><main></main>`;
     }
 }
 
 customElements.define('app-layout', AppLayout);
+
+class Modal extends s {
+    static styles = r$2`:host{position:fixed;top:0;left:0;width:100%;height:100%;display:flex;justify-content:center;align-items:center;background:rgba(0,0,0,.25)}.inner-modal{position:absolute;background:#fff;padding:var(--big-padding);border-radius:var(--border-radius);width:550px;max-width:82%;min-height:32px;word-break:break-word}.close-modal{color:#aaa;line-height:var(--big-padding);position:absolute;right:0;top:0;text-align:center;width:70px}.close-modal:hover{cursor:pointer;color:var(--color-on-background)}`;
+
+    constructor() {
+        super();
+    }
+
+    toggle() {
+        const isOpen = this.getAttribute('data-open') === 'true';
+
+        if (isOpen) {
+            return this.close();
+        }
+
+        return this.open();
+    }
+
+    open() {
+        const isOpen = this.getAttribute('data-open') === 'true';
+
+        if (isOpen) {
+            return;
+        }
+
+        requestAnimationFrame(() => {
+            this.classList.remove('hide');
+            return this.setAttribute('data-open', 'true');
+        });
+    }
+
+    close() {
+        const isClosed = this.getAttribute('data-open') === 'false';
+
+        if (isClosed) {
+            return;
+        }
+
+        requestAnimationFrame(() => {
+            this.classList.add('hide');
+            return this.setAttribute('data-open', 'false');
+        });
+    }
+
+    set({ HTML }) {
+        console.log(this);
+        this.shadowRoot.querySelector('.modal-content').innerHTML = HTML;
+    }
+
+    setAndOpen(options) {
+        this.set(options);
+        this.open();
+    }
+
+    render() {
+        return $`<div class="inner-modal"><small @click="${this.close}" class="close-modal">close</small><div class="modal-content"></div></div>`;
+    }
+}
+
+customElements.define('modal-window', Modal);
+
+document.addEventListener('DOMContentLoaded', function () {
+    const modal = document.querySelector('modal-window');
+
+    modal.setAndOpen({
+        HTML: 'willkomen im lit template',
+    });
+});
